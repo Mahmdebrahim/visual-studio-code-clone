8c7e4b960543772fdd70946e77bfd95f5aa8df76
Mahmdebrahim <muhammedebrahim712.com>
2025-09-28 16:27:14 +0300

    resizablePanel

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import "./App.css";
 import { fileTree } from "./data";
 import RecursiveComponent from "./components/RecursiveComponent";
 import OpendFilesBar from "./components/OpendFilesBar";
+import ResizablePanel from "./components/ResizablePanel";
 
 function App() {
   return (
-    <div className="">
-      <div className="flex w-screen h-screen">
-        <div className="w-64 p-2 border-r-[1px] border-[#ffffff1f]">
+    <div className="flex h-screen">
+    <ResizablePanel
+      showLeftPanel
+      leftPanel={
+        <div className="w-64 p-2">
           <RecursiveComponent file={fileTree} />
         </div>
-        <div className="flex-1 flex flex-col">
-          <OpendFilesBar/>
-        </div>
-      </div>
-    </div>
+      }
+      rightPanel={<OpendFilesBar/>}
+    />
+  </div> 
   );
 }
 
---
+++ src/components/ResizablePanel.tsx
@@ -0,0 +1,34 @@
+import type { ReactNode } from "react";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+
+interface IProps {
+  defaultLayout?: number[] | undefined;
+  leftPanel: ReactNode;
+  rightPanel: ReactNode;
+  showLeftPanel: boolean;
+}
+
+const ResizablePanel = ({
+  defaultLayout = [80, 20],
+  leftPanel,
+  rightPanel,
+  showLeftPanel,
+}: IProps) => {
+  const onLayout = (sizes: number[]) => {
+    document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`;
+  };
+  return (
+    <PanelGroup direction="horizontal" onLayout={onLayout} autoSaveId="condition">
+      {showLeftPanel && (
+        <>
+          <Panel defaultSize={defaultLayout[0]} collapsible={true}>{leftPanel}</Panel>
+          <PanelResizeHandle className="border-r-2 border-[#ffffff1f]" />
+        </>
+      )}
+
+      <Panel defaultSize={defaultLayout[1]}>{rightPanel}</Panel>
+    </PanelGroup>
+  );
+};
+
+export default ResizablePanel;
